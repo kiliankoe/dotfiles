c29bc5cbefba04d29ab389ce8a2cf3a9d9c79a2a
Kilian Koeltzsch <me@kilian.io>
2015-09-27 17:11:09 +0200

    no need for this if init.sh is part of repo

--- init.sh
+++ init.sh
@@ -8,9 +8,6 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Clone dotfiles from GitHub
-git clone git@github.com:kiliankoe/dotfiles.git ~/dotfiles
-
 # Symlink files
 ln -s /Users/kilian/dotfiles/.gemrc /Users/kilian/.gemrc
 ln -s /Users/kilian/dotfiles/bash_profile /Users/kilian/.bash_profile
